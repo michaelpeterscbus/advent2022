7c5b9ad075e6b9a13618832a37ea418a83967db8
michaelpeterscbus <32644231+michaelpeterscbus@users.noreply.github.com>
2022-12-01 10:28:42 -0500

    day 1

---
+++ src/main/java/advent2022/Day1.java
@@ -0,0 +1,46 @@
+package advent2022;
+
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Day1 {
+    public int getMaxCalories(String filename) {
+        var caloriesPerElf = getCalorieList(filename);
+        return caloriesPerElf.stream().max(Comparator.comparingInt(Integer::intValue)).get();
+    }
+
+    public int getTop3CalorieTotal(String filename) {
+        var caloriesPerElf = getCalorieList(filename);
+        var sortedCalories = caloriesPerElf.stream().sorted(Comparator.comparingInt(Integer::intValue).reversed());
+        return sortedCalories.limit(3).mapToInt(Integer::intValue).sum();
+    }
+
+    @SneakyThrows
+    private List<Integer> getCalorieList(String filename) {
+        var reader = new BufferedReader(new FileReader("src/test/resources/day1/" + filename));
+        String line;
+        int index = 0;
+        var caloriesPerElf = new ArrayList<Integer>();
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()) {
+                index++;
+            } else {
+                if (index > caloriesPerElf.size() - 1) {
+                    caloriesPerElf.add(Integer.parseInt(line));
+                } else {
+                    var total = caloriesPerElf.get(index);
+                    total += Integer.parseInt(line);
+                    caloriesPerElf.set(index, total);
+                }
+            }
+        }
+        return caloriesPerElf;
+    }
+}
---
+++ src/test/java/advent2022/Day1Test.java
@@ -0,0 +1,35 @@
+package advent2022;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day1Test {
+
+    private Day1 subject = new Day1();
+
+    @Test
+    void part1_withSampleInput() {
+        var result = subject.getMaxCalories("sampleInput.txt");
+
+        assertThat(result).isEqualTo(24000);
+    }
+
+    @Test
+    void part1_answer() {
+        System.out.println(subject.getMaxCalories("input.txt"));
+    }
+
+    @Test
+    void part2_withSampleInput() {
+        var result = subject.getTop3CalorieTotal("sampleInput.txt");
+
+        assertThat(result).isEqualTo(45000);
+    }
+
+    @Test
+    void part2_answer() {
+        System.out.println(subject.getTop3CalorieTotal("input.txt"));
+    }
+}
