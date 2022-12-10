c709762147142bc45ca89b4c432fb0fd09fcfc5e
michaelpeterscbus <32644231+michaelpeterscbus@users.noreply.github.com>
2022-12-10 13:46:58 -0500

    day 8 pt 1

--- src/main/java/advent2022/Day8.java
+++ src/main/java/advent2022/Day8.java
@@ -2,11 +2,8 @@ package advent2022;
 
 import lombok.SneakyThrows;
 
-import java.awt.*;
 import java.io.File;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.HashSet;
 
 public class Day8 {
 
@@ -21,46 +18,46 @@ public class Day8 {
             }
         }
         var total = 0;
-        var map = new HashMap<Point, Boolean>();
         for (int row = 0; row < forest.length; row++) {
             for (int col = 0; col < forest[0].length; col++) {
-                var visited = new HashSet<Point>();
-                var point = new Point(row, col);
-                if (isVisible(forest, row, col, Integer.MAX_VALUE, visited)) {
-                    map.put(point, true);
+                if (isVisible(forest, row, col)) {
                     total++;
-                }
-                else {
-                    System.out.println(row + " " + col);
-                    map.put(point, false);
                 }
             }
         }
         return total;
     }
 
-    private boolean isVisible(int[][] forest, int row, int col, int prevTreeSize, HashSet<Point> visited) {
-        if (!isInbounds(forest, row, col)) return true;
-
-        var point = new Point(row, col);
-        if (visited.contains(point)) return false;
-
-        visited.add(point);
+    private boolean isVisible(int[][] forest, int row, int col) {
         var currentSize = forest[row][col];
-
-        if (prevTreeSize <= currentSize) return false;
-
-        var visibleFromBelow = isVisible(forest, row + 1, col, currentSize, visited);
-        var visibleFromAbove=  isVisible(forest, row - 1, col, currentSize, visited);
-        var visibleFromRight= isVisible(forest, row, col + 1, currentSize, visited);
-        var visibleFromLeft = isVisible(forest, row, col - 1, currentSize, visited);
-
+        var visibleFromBelow = true;
+        for (int i = row  + 1; i < forest.length; i++) {
+            if (forest[i][col] >= currentSize) {
+                visibleFromBelow = false;
+                break;
+            }
+        }
+        var visibleFromAbove=  true;
+        for (int i = row - 1; i >= 0; i--) {
+            if (forest[i][col] >= currentSize) {
+                visibleFromAbove = false;
+                break;
+            }
+        }
+        var visibleFromRight = true;
+        for (int i = col + 1; i < forest[0].length; i++) {
+            if (forest[row][i] >= currentSize) {
+                visibleFromRight = false;
+                break;
+            }
+        }
+        var visibleFromLeft = true;
+        for (int i = col - 1; i >= 0; i--) {
+            if (forest[row][i] >= currentSize) {
+                visibleFromLeft = false;
+                break;
+            }
+        }
         return visibleFromAbove || visibleFromBelow || visibleFromLeft || visibleFromRight;
-    }
-
-    private boolean isInbounds(int[][] forest, int row, int col) {
-        if (row < 0 || row >= forest.length) return false;
-        if (col < 0 || col >= forest[0].length) return false;
-        return true;
     }
 }
